87a12465f9785456d5db1628d8dc252f132c9543
scarter31191 <scarter31191@gmaile.com>
2021-03-16 22:51:42 -0400

    commit tree a222d31407abf53bfabe1efc0f7993253ac87ab9

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Add your code here
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+
+// let configObj = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify(formData)
+// }; 
+
+// fetch("http://localhost:3000/dogs", configObj)
+// .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(object) {
+//     console.log(object);
+//   })
+//   .catch(function(error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   }); 
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+       method: 'POST', 
+       headers: {
+           "Content-Type": 'application/json',
+           "Accept": 'application/json'
+       },
+       body: JSON.stringify( {
+           name,
+           email
+       }) 
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(object){
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    })
+}
